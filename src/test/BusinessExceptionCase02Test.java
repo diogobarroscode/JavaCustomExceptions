cd172905155e14adc54e673c18af8bc85a5e231a
diogobarroscode <diogobarros.code@gmail.com>
2024-01-31 00:35:27 -0300

    added test classes to check success and error cases

---
+++ src/test/BusinessExceptionCase02Test.java
@@ -0,0 +1,31 @@
+package test;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+public class BusinessExceptionCase02Test {
+
+	public static void main(String[] args) {
+		
+		int number = 8021;
+		String holder = "Diogo Barros";
+		double balance = 500.00;
+		double withdrawLimit = 300.00;
+		double amount = 400.00;
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		try {
+			
+			acc.withdraw(amount);
+			System.out.printf("Saque realizado com sucesso! \nSaldo atual: %.2f%n", acc.getBalance());
+			
+		} catch (BusinessException e) {
+			
+			System.err.println(e.getMessage());
+			
+		}
+
+	}
+
+}
---
+++ src/test/BusinessExceptionCase04Test.java
@@ -0,0 +1,29 @@
+package test;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+public class BusinessExceptionCase04Test {
+	public static void main(String[] args) {
+		
+		int number = 8021;
+		String holder = "Diogo Barros";
+		double balance = 200.00;
+		double withdrawLimit = 300.00;
+		double amount = 250.00;
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		try {
+			
+			acc.withdraw(amount);
+			System.out.printf("Saque realizado com sucesso! \nSaldo atual: %.2f%n", acc.getBalance());
+			
+		} catch (BusinessException e) {
+			
+			System.err.println(e.getMessage());
+			
+		}
+
+	}
+}
